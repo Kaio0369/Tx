2b3385bb872df232ac3e470a11a4fd94df286a3d
Kaio0369 <140551086+Kaio0369@users.noreply.github.com>
2024-07-07 20:19:15 -0300

    Delete codigo.js

--- codigo.js
+++
@@ -1,4 +0,0 @@
-<div id="card-projetos" class="card-section" style="display: none;">
-    <ion-icon name="arrow-back-circle-outline" class="back-icon" onclick="exitCards()"></ion-icon>
-    <h1 class="card-titulos">Portfólio</h1>
-</div> 
